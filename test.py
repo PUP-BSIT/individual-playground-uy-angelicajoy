0ce0b7984446713afb191b9c7213420db1e2c6e6
uy-angelicajoy <angelicajoyuy16@gmail.com>
2024-09-30 19:46:15 +0800

    update Python test file

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-print ("This is File 1")
-print (50)
+first_name = "Angelica"
+age=20
+type = "New Patient"
+is_online = False
+print (first_name, age, type)
+
+name = input("What is your name? ")
+print("Hello " + name)
